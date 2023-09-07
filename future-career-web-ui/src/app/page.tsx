e20bc80057653004f159d42464718b87290ad50f
SemukeloMathe <semmathe@gmail.com>
2023-09-07 22:53:05 +0200

    modified home page and styles

--- future-career-web-ui/src/app/page.tsx
+++ future-career-web-ui/src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home Page</h1>
-      </main>
-  )
+    <>
+      <div className={styles.heading}>
+        <h1>Home Page</h1>
+      </div>
+    </>
+  );
 }
